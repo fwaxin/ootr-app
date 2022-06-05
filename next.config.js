929a31a2515b42ce0cf9295ea9e530f274c1510f
François WAXIN <francois.waxin@icloud.com>
2022-06-05 21:42:26 +0200

    Retrieve and parse markdown files from a folder to a given page

--- next.config.js
+++ next.config.js
@@ -4,6 +4,13 @@ const nextConfig = {
   images: {
     domains: ['cdn.discordapp.com'],
   },
+  webpack: function(config) {
+    config.module.rules.push({
+      test: /\.md$/i,
+      type: 'asset/source',
+    });
+    return config
+  }
 }
 
 module.exports = nextConfig
---
+++ pages/wiki/[slug].tsx
@@ -0,0 +1,52 @@
+import fs from "fs";
+import matter from "gray-matter";
+import remarkGfm from "remark-gfm";
+import ReactMarkdown from "react-markdown";
+
+const WikiContentPage = ({ ...props }) => {
+  return (
+    <>
+      <h1>{props.title}</h1>
+      <main>
+        <ReactMarkdown remarkPlugins={[remarkGfm]}>
+          {props.markdownBody}
+        </ReactMarkdown>
+      </main>
+    </>
+  );
+};
+
+export const getStaticProps = async ({ ...context }) => {
+  const { slug } = context.params;
+  const fileContents = await import(`/data/wiki/${slug}.md`);
+  const { data, content } = matter(fileContents.default);
+
+  if (!data || !content) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      title: data.title,
+      markdownBody: content,
+    },
+  };
+};
+
+export const getStaticPaths = async () => {
+  const wikiFiles = fs.readdirSync("data/wiki");
+  const filePaths = wikiFiles.map((fileName) => ({
+    params: {
+      slug: fileName.replace(/\.md$/, ""),
+    },
+  }));
+
+  return {
+    paths: filePaths,
+    fallback: false,
+  };
+};
+
+export default WikiContentPage;
