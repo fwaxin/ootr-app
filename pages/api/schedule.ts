57091efa38edcbe5f6b56fcb38803215d99ff02e
François WAXIN <francois.waxin@icloud.com>
2022-06-25 20:31:51 +0200

    Begnning fetching schedule info from Cybrou's OoTR Ladder bot's webservice

---
+++ pages/api/schedule.ts
@@ -0,0 +1,21 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+const ladderSchedule = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const scheduleInformations = await fetch(
+      `${process.env.LADDER_BOT_HOSTNAME}/session/schedule`,
+      {
+        method: "GET",
+        headers: {
+          "X-ApiKey": `${process.env.LADDER_BOT_API_KEY}`,
+        },
+      }
+    ).then((res) => res.json());
+
+    res.send(scheduleInformations);
+  } catch (e) {
+    res.redirect("/?r=false");
+  }
+};
+
+export default ladderSchedule;
