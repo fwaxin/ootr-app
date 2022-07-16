353a8da77b96fba954273b28e0227d3b022f2578
François WAXIN <francois.waxin@icloud.com>
2022-07-16 23:02:12 +0200

    New Timer component to use it for currently running sessions

---
+++ components/Timer.tsx
@@ -0,0 +1,57 @@
+import { FC, useEffect, useMemo, useState } from 'react';
+
+import { Typography } from '@mui/material';
+import { differenceInMilliseconds, getTime } from 'date-fns';
+
+import { ONE_HOUR, ONE_MINUTE } from 'constant';
+
+interface TimerProps {
+  startDate: Date;
+}
+
+const Timer: FC<TimerProps> = ({ startDate }) => {
+  const [currentTime, setCurrentTime] = useState<number>(getTime(new Date()));
+  const diffInMillis = differenceInMilliseconds(currentTime, startDate);
+
+  const getTimer = () => {
+    if (diffInMillis < 1) {
+      return null;
+    }
+
+    const hours = Math.floor(diffInMillis / (1000 * ONE_HOUR));
+    const minutes = Math.floor((diffInMillis - hours * (1000 * ONE_HOUR)) / (1000 * ONE_MINUTE));
+    const seconds = Math.floor((diffInMillis - hours * (1000 * ONE_HOUR) - minutes * (1000 * ONE_MINUTE)) / 1000);
+    const milliseconds = Math.floor(
+      diffInMillis - hours * (1000 * ONE_HOUR) - minutes * (1000 * ONE_MINUTE) - seconds * 1000,
+    );
+
+    return {
+      hours,
+      minutes,
+      seconds,
+      milliseconds,
+    };
+  };
+
+  const timer = useMemo(getTimer, [currentTime]);
+
+  useEffect(() => {
+    setInterval(() => {
+      const now = getTime(new Date());
+      setCurrentTime(now);
+    }, 10);
+  }, []);
+
+  return (
+    <>
+      {timer && (
+        <Typography variant="overline" lineHeight={1}>
+          {timer.hours.toString().padStart(1, '0')}:{timer.minutes.toString().padStart(2, '0')}:
+          {timer.seconds.toString().padStart(2, '0')}.{timer.milliseconds.toString().slice(0, 2)}
+        </Typography>
+      )}
+    </>
+  );
+};
+
+export default Timer;
