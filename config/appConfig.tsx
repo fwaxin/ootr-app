03d167a8366838fcea1c470d8b76a2931c3de742
François WAXIN <francois.waxin@icloud.com>
2022-09-22 21:00:18 +0200

    feat(leaderboard): Make leaderboard page dynamic depending on ladder type in URL

--- config/appConfig.tsx
+++ config/appConfig.tsx
@@ -2,8 +2,12 @@ import ComputerIcon from 'components/icons/ComputerIcon';
 import MoonIcon from 'components/icons/MoonIcon';
 import SunIcon from 'components/icons/SunIcon';
 
+export const DEFAULT_LADDER_TYPE: string = 'standard;';
+
 export const LADDER_CAT_READABLE_NAME: any = {
   lightShuffle: 'Shuffle (light)',
+  standard: 'Standard',
+  shuffle: 'Shuffle',
 };
 
 export const APP_THEME_CONF = [
